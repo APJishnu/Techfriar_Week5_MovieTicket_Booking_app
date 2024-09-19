f9c33bb72b12d2d109806606744c866705fffe75
APJishnu <apjishnu23@gmail.com>
2024-09-19 11:18:07 +0530

    text commit

--- server/src/app.js
+++ server/src/app.js
@@ -41,7 +41,7 @@ app.use(session({
   cookie: {
     secure: process.env.NODE_ENV === 'production', // true in production (for HTTPS), false in development
     httpOnly:  process.env.NODE_ENV === 'development',
-    maxAge: 1000 * 60 * 15
+    maxAge: 3600 * 1000
   }
 }));
 
