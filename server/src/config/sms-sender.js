fef484e44940032b921f37c1b33dcc206be540ce
APJishnu <124502525+APJishnu@users.noreply.github.com>
2025-12-19 14:53:51 +0530

    Refactor sendWhatsAppMessage for better clarity
    Refactor sendWhatsAppMessage function to improve readability and structure.

--- server/src/config/sms-sender.js
+++ server/src/config/sms-sender.js
@@ -1,21 +1,22 @@
 const twilio = require("twilio");
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
-const sendWhatsAppMessage = async (phoneNumber, messageBody, mediaUrl) => {
-  try {
-    const message = await client.messages.create({
-      from: "whatsapp:+14155238886", // Use your Twilio WhatsApp number
-      to: `whatsapp:+91${phoneNumber}`, // Format phone number for WhatsApp
-      body: messageBody,
-      mediaUrl: [mediaUrl], // Include the media URL here
-    });
-    return message;
-  } catch (error) {
-    throw new Error(`Failed to send WhatsApp message: ${error.message}`);
+async function sendWhatsAppMessage(phoneNumber, messageBody, mediaUrl) {
+  const payload = {
+    from: "whatsapp:+14155238886",
+    to: `whatsapp:+91${phoneNumber}`,
+    body: messageBody,
+  };
+
+  if (mediaUrl) {
+    payload.mediaUrl = [mediaUrl];
   }
-};
+
+  return await client.messages.create(payload);
+}
 
 module.exports = { sendWhatsAppMessage };
